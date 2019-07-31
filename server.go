f2d011c042c3bc41951f7e0d477b1bfed858b592
Ruben Fonseca <fonseka@gmail.com>
2019-07-31 11:45:10 +0200

    Refactor authorization into middleware.

--- server.go
+++ server.go
@@ -23,64 +23,76 @@ func (s *Server) Start() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+func (s *Server) authorizeHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ok, err := s.authorizer.Authorize(r)
+
+		if err != nil {
+			http.Error(w, "Internal authorization error", http.StatusInternalServerError)
+			return
+		}
+
+		if !ok {
+			http.Error(w, "Not authorized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (s *Server) handleUpload() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check path presence
+		if r.URL.Path == "" {
+			http.Error(w, "Missing object key", http.StatusBadRequest)
+			return
+		}
+
+		// Check that we have something to upload
+		if r.Body == nil {
+			http.Error(w, "Empty body", http.StatusBadRequest)
+			return
+		}
+
+		// Check request size, if possible
+		if r.ContentLength > 0 && r.ContentLength > s.maxBodySize {
+			http.Error(w, "Request too big", http.StatusRequestEntityTooLarge)
+			return
+		}
+
+		// Clients might still cheat, so create a reader that doesn't read more than allowed.
+		body := http.MaxBytesReader(w, r.Body, s.maxBodySize)
+
+		// Ask the storage backend for an object
+		object := s.storage.PutObject(r.URL.Path)
+		if object == nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		// Pump bytes from http to storage
+		_, err := io.Copy(object, body)
+		if err != nil {
+			log.Printf("Error pumping bytes: %s", err.Error())
+			http.Error(w, "Storage error", http.StatusInternalServerError)
+			return
+		}
+
+		// Tell the storage backend that we are done with the transfer.
+		err = object.Close()
+		if err != nil {
+			log.Printf("Error finalizing file: %s", err.Error())
+			http.Error(w, "Storage error", http.StatusInternalServerError)
+			return
+		}
+
+		// If we get here, the upload succeeded, so just tell the client everything's OK
+		w.WriteHeader(200)
+		_, _ = w.Write([]byte("OK"))
+	})
+}
+
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Authorization
-	ok, err := s.authorizer.Authorize(r)
-	if err != nil {
-		http.Error(w, "Internal authorization error", http.StatusInternalServerError)
-		return
-	}
-
-	if !ok {
-		http.Error(w, "Not authorized", http.StatusUnauthorized)
-		return
-	}
-
-	// Check path presence
-	if r.URL.Path == "" {
-		http.Error(w, "Missing object key", http.StatusBadRequest)
-		return
-	}
-
-	// Check that we have something to upload
-	if r.Body == nil {
-		http.Error(w, "Empty body", http.StatusBadRequest)
-		return
-	}
-
-	// Check request size, if possible
-	if r.ContentLength > 0 && r.ContentLength > s.maxBodySize {
-		http.Error(w, "Request too big", http.StatusRequestEntityTooLarge)
-		return
-	}
-
-	// Clients might still cheat, so create a reader that doesn't read more than allowed.
-	body := http.MaxBytesReader(w, r.Body, s.maxBodySize)
-
-	// Ask the storage backend for an object
-	object := s.storage.PutObject(r.URL.Path)
-	if object == nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	// Pump bytes from http to storage
-	_, err = io.Copy(object, body)
-	if err != nil {
-		log.Printf("Error pumping bytes: %s", err.Error())
-		http.Error(w, "Storage error", http.StatusInternalServerError)
-		return
-	}
-
-	// Tell the storage backend that we are done with the transfer.
-	err = object.Close()
-	if err != nil {
-		log.Printf("Error finalizing file: %s", err.Error())
-		http.Error(w, "Storage error", http.StatusInternalServerError)
-		return
-	}
-
-	// If we get here, the upload succeeded, so just tell the client everything's OK
-	w.WriteHeader(200)
-	_, _ = w.Write([]byte("OK"))
+	s.authorizeHandler(s.handleUpload()).ServeHTTP(w, r)
 }
